7d8ea7193b938d5858cd65c5eadd6275252aadc3
xmiwon <procrutivate@outlook.com>
2021-03-10 13:51:41 +0100

    New features

---
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './ImageLinkForm.css'
+
+const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
+    return (
+        <div className='ma4 mt0'>
+            <p className='f3'>This Magic Brain will detect faces in your pictures. Git it a try.
+            </p>
+            <p>Single face sample: https://upload.wikimedia.org/wikipedia/commons/8/85/Elon_Musk_Royal_Society_%28crop1%29.jpg</p>
+            <p>Multi face sample: https://static01.nyt.com/images/2019/10/02/video/02-still-for-america-room-loop/02-still-for-america-room-loop-superJumbo.jpg</p>
+            <div className='center'>
+                <div className='form center pa4 br3 shadow-5'>
+                    <input className='f4 pa2 w-70 center' type='text' placeholder='Enter link here' onChange={onInputChange} />
+                    <button 
+                    className='w-30 grow f4 link ph3 pv2 dib white bg-light-blue' 
+                    onClick={onButtonSubmit}>
+                    
+                    Detect</button>
+                    
+                    
+                </div>
+
+            </div>
+        </div>
+    );
+}
+export default ImageLinkForm;
